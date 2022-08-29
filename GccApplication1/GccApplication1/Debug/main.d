305f95bd6a7c64ddf9898e1900e2e46dd1531e81
George Welson <94200368+WelsonAA@users.noreply.github.com>
2022-08-30 01:50:27 +0200

    Add files via upload

---
+++ GccApplication1/GccApplication1/Debug/main.d
@@ -0,0 +1,43 @@
+main.d main.o: .././main.c \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ D:\Edge\ Downloads\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom32.h \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+D:\Edge\ Downloads\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom32.h:
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\edge\ downloads\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,19 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 29-Aug-22 8:35:38 PM
+ * Author : George Welson
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 1000000U
+#include <util/delay.h>
+
+int main(void){
+	DDRB |=(1<<3);
+	while(1){
+		PORTB ^= (1<<3);
+		_delay_ms(2000);
+	}
+}
+
